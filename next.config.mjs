be7c8deeb8cbeb280866e5c60e103bef8c764250
Paulo Holanda <paulorfholanda@gmail.com>
2024-07-26 19:58:52 -0300

    chore: Update project configuration files and add trailing slash to URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-/*
-export default nextConfig;
-module.exports = {
+const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/PauloFH' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/PauloFH/' : '',
-}
-  */
+  trailingSlash: true, // Opcional: Adiciona uma barra no final das URLs
+  // Outras configurações que você possa precisar
+};
+
+module.exports = nextConfig;
